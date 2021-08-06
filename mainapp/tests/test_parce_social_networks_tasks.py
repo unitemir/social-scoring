e14686e7a40540d8fb806e76e7adcf809046be58
Islam <4866457@gmail.com>
2021-08-06 16:03:58 +0600

    ref

--- mainapp/tests/test_parce_social_networks_tasks.py
+++ mainapp/tests/test_parce_social_networks_tasks.py
@@ -3,7 +3,19 @@ from ..models import Person
 from ..tasks import *
 
 
+def test_successful_parce_instagram(db):
+    create_new_instagram_user()
+    inst_person = Person.objects.all().reverse()[0]
+    assert inst_person.social_network == 'Instagram'
+
+
 def test_successful_parce_vk(db):
-    create_new_vk_person('id165028790')
-    person_vk = Person.objects.all().reverse()[0]
-    assert person_vk.social_network == 'VK'
+    create_new_vk_person()
+    vk_person = Person.objects.all().reverse()[0]
+    assert vk_person.social_network == 'VK'
+
+
+def test_successful_parce_facebook(db):
+    get_facebook_friends_list()
+    facebook_person = Person.objects.all().reverse()[0]
+    assert facebook_person.social_network == 'Facebook'
