114866568d1eccc4ccc5ad441c8fed011abc4cf5
Islam <4866457@gmail.com>
2021-08-11 15:13:39 +0600

    added function for calculating score user vk

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,6 +6,8 @@ from .tasks import create_new_instagram_user, create_new_vk_person, get_facebook
 from rest_framework.response import Response
 
 import requests
+from django.shortcuts import render
+from .models import Person
 
 
 class CreateInstagramAPIView(APIView):
@@ -29,99 +31,88 @@ class CreateFacebookAPIView(APIView):
         return Response({'test': 'Facebook task has been running'})
 
 
-
 class GetVKScoreAPIView(APIView):
 
     def get(self, request, vk_id):
+        access_token = '23c80a4e23c80a4e23c80a4ee523b0e924223c823c80a4e42d9073819b94c55e72fc001'
+
         input_data = vk_id
-        get_user = f'https://api.vk.com/method/users.get?user_ids={input_data}&v=5.92&access_token=23c80a4e23c80a4e23c80a4ee523b0e924223c823c80a4e42d9073819b94c55e72fc001'
-        user_id = requests.get(get_user).json()['response'][0]['id']
+        get_vk_user_id_url = f'https://api.vk.com/method/users.get?user_ids={input_data}&v=5.92&access_token={access_token}'
+        vk_user_id = requests.get(get_vk_user_id_url).json()['response'][0]['id']
 
-        get_user_friedns = f'https://api.vk.com/method/friends.get?user_id={user_id}&fields=domain&v=5.92&access_token=23c80a4e23c80a4e23c80a4ee523b0e924223c823c80a4e42d9073819b94c55e72fc001'
-
-        response = requests.get(get_user_friedns).json()['response']['items']
-
-        scammer = input_data
+        get_user_friends_list_url = f'https://api.vk.com/method/friends.get?user_id={vk_user_id}&fields=domain&v=5.92&access_token={access_token}'
+        scammer_friends_list = requests.get(get_user_friends_list_url).json()['response']['items']
 
         data = {}
 
-        for friend in response:
-            data[friend['domain']] = []
-            inp_data = friend['domain']
-            get_user = f'https://api.vk.com/method/users.get?user_ids={inp_data}&v=5.92&access_token=23c80a4e23c80a4e23c80a4ee523b0e924223c823c80a4e42d9073819b94c55e72fc001'
-            sub_flist = requests.get(get_user).json()['response'][0]['id']
-            get_sub_flist_friedns = f'https://api.vk.com/method/friends.get?user_id={sub_flist}&fields=domain&v=5.92&access_token=23c80a4e23c80a4e23c80a4ee523b0e924223c823c80a4e42d9073819b94c55e72fc001'
+        scammer_model_object = Person.objects.create(username=input_data, score=1)
+
+        for scammer_friend in scammer_friends_list:
             try:
-                response = requests.get(get_sub_flist_friedns).json()
-                response = response['response']['items']
-                for sub_friend in response:
-                    data[friend['domain']].append(sub_friend['domain'])
+                scammer_friend_model_object = Person.objects.get(username=scammer_friend['domain'])
+            except:
+                scammer_friend_model_object = Person.objects.create(username=scammer_friend['domain'], score=0.5, parent=scammer_model_object)
+            scammer_friend_vk_domain = scammer_friend['domain']
+            get_scammer_friend_vk_id_url = f'https://api.vk.com/method/users.get?user_ids={scammer_friend_vk_domain}&v=5.92&access_token={access_token}'
+            scammer_friend_vk_id = requests.get(get_scammer_friend_vk_id_url).json()['response'][0]['id']
+            data[scammer_friend_vk_domain] = []
+            get_scammer_friend_friends_list_url = f'https://api.vk.com/method/friends.get?user_id={scammer_friend_vk_id}&fields=domain&v=5.92&access_token={access_token}'
+            try:
+                scammer_friend_friends_list = requests.get(get_scammer_friend_friends_list_url).json()['response'][
+                    'items']
+                for scammer_friend_friend in scammer_friend_friends_list:
+                    try:
+                        Person.objects.get(username=scammer_friend_friend['domain'])
+                    except:
+                        if scammer_friend_friend != input_data:
+                            Person.objects.create(
+                                username=scammer_friend_friend['domain'],
+                                score=0.25,
+                                parent=scammer_friend_model_object
+                            )
+                    data[scammer_friend_vk_domain].append(scammer_friend_friend['domain'])
             except:
                 continue
 
-        vertices = [scammer]
+        # scammer = input_data
+        #
+        # vertices = [scammer]
+        #
+        # for scammer_friend, scammer_friend_friends_list in data.items():
+        #     vertices.append(scammer_friend)
+        #     for scammer_friend_friend in scammer_friend_friends_list:
+        #         if scammer_friend_friend == scammer:
+        #             continue
+        #         vertices.append(scammer_friend_friend)
+        #
+        # vertices.append(scammer)
+        #
+        # graph = {person: list() for person in vertices}
 
-        for scammer_friend, scammer_friend_sub_friends in data.items():
-            vertices.append(scammer_friend)
-            for sub_friend in scammer_friend_sub_friends:
-                if sub_friend == scammer:
-                    continue
-                vertices.append(sub_friend)
-
-        graph = {person: list() for person in vertices}
-
-        for friend, sub_friends in data.items():
-            for sub_friend in sub_friends:
-                graph[friend].append(sub_friend)
-                graph[sub_friend].append(friend)
+        # for friend, friend_friends in data.items():
+        #     graph[scammer].append(friend)
+        #     for friend_friend in friend_friends:
+        #         if friend_friend == scammer:
+        #             continue
+        #         graph[friend].append(friend_friend)
 
         # print(graph)
 
-        # start_vertex = scammer
-        # queue = deque([start_vertex])
-        # x = 0
-        # while x < len(vertices):
-        #     try:
-        #         cur_v = queue.popleft()
-        #     except:
-        #         pass
+        # visited = []
+        # queue = []
         #
-        #     if x == 1:
-        #         for heigh_v in graph[cur_v]:
-        #             try:
-        #                 scammer_obj = Person.objects.get(name=cur_v)
-        #             except:
-        #                 scammer_obj = Person.objects.create(name=cur_v, score=1)
+        # visited.append(scammer)
+        # queue.append(scammer)
         #
-        #             try:
-        #                 Person.objects.get(name=heigh_v)
-        #             except:
-        #                 Person.objects.create(name=heigh_v, score=0.5, parent=scammer_obj)
-        #
-        #             queue.append(heigh_v)
-        #
-        #     print(x, cur_v)
-        #
-        #     for heigh_v in graph[cur_v]:
-        #         print(heigh_v, 'heigh')
-        #         queue.append(heigh_v)
+        # while queue:
+        #     s = queue.pop(0)
+        #     for neighbour in graph[s]:
+        #         if neighbour not in visited:
+        #             visited.append(neighbour)
+        #             queue.append(neighbour)
 
-        start_vertex = scammer
-        queue = deque([start_vertex])
-        x = 0
-        while x < len(vertices):
-            cur_v = queue.popleft()
-            print(cur_v, 'cur_v')
-            print(x)
-
-            for heigh_v in graph[cur_v]:
-                print(heigh_v, 'heigh_v')
-                queue.append(heigh_v)
-            else:
-                x += 1
-
-        return Response({'status': 'success'})
+        return Response({'test': 'response'})
 
 
-def show_persons_three(request):
+def show_persons(request):
     return render(request, "persons_three.html", {'persons': Person.objects.all()})
