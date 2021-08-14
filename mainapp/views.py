7242c90effd16bcd0e7765eb180aac8058fcd6b1
Islam <4866457@gmail.com>
2021-08-14 14:53:32 +0600

    refactoring

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,7 @@
 from rest_framework.views import APIView
-
-from django.http import HttpResponse
-
-from .tasks import *
 from rest_framework.response import Response
 
-import requests
-from django.shortcuts import render
-from .models import Person
+from .tasks import *
 
 
 class GetSocialNetworksStatsView(APIView):
@@ -15,10 +9,10 @@ class GetSocialNetworksStatsView(APIView):
     def get(self, request, social_network, username):
         if social_network == 'Instagram':
             create_new_instagram_user.delay(username)
-            return Response({'test': 'Instagram task has been running'})
+            return Response({'response': 'Instagram task has been running'})
         if social_network == 'Facebook':
             get_friends.delay(username)
-            return Response({'test': 'Facebook task has been running'})
+            return Response({'response': 'Facebook task has been running'})
         if social_network == 'VK':
             create_new_vk_person.delay(username)
-            return Response({'test': 'VK task has been running'})
+            return Response({'response': 'VK task has been running'})
