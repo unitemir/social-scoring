dc4c5ea31eec486ba4cabcffc34a659bafd89375
Islam <4866457@gmail.com>
2021-08-14 09:31:42 +0600

    removed generate random user-agent in facebook parsing function

--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -15,9 +15,6 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
-
 
 @app.task()
 def create_new_instagram_user(insta_username):
@@ -93,20 +90,12 @@ def create_new_vk_person(page_id):
 def get_friends(facebook_id):
     chrome_options = Options()
 
-    software_names = [SoftwareName.CHROME.value]
-    operating_systems = [OperatingSystem.WINDOWS.value,
-                         OperatingSystem.LINUX.value]
-    user_agent_rotator = UserAgent(software_names=software_names,
-                                   operating_systems=operating_systems,
-                                   limit=100)
-    user_agent = user_agent_rotator.get_random_user_agent()
-
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--window-size-1420,1080')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument(f'user-agent={user_agent}')
+    # chrome_options.add_argument(f'user-agent={}')
     chrome_options.add_argument("--disable-javascript")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
@@ -170,20 +159,12 @@ def create_new_facebook_person(facebook_id, number_of_friends):
 
     chrome_options = Options()
 
-    software_names = [SoftwareName.CHROME.value]
-    operating_systems = [OperatingSystem.WINDOWS.value,
-                         OperatingSystem.LINUX.value]
-    user_agent_rotator = UserAgent(software_names=software_names,
-                                   operating_systems=operating_systems,
-                                   limit=100)
-    user_agent = user_agent_rotator.get_random_user_agent()
-
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--window-size-1420,1080')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument(f'user-agent={user_agent}')
+    # chrome_options.add_argument(f'user-agent={}')
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
