e4f77d8c21503743db5dd1fd84c884335b949f96
Islam <4866457@gmail.com>
2021-08-24 12:34:58 +0600

    added PersonStats model

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,17 +7,22 @@ class Person(MPTTModel):
     full_name = models.CharField('ФИО', max_length=128)
     score = models.FloatField('Рейтинг', null=True, blank=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
-    qty_subscribers = models.PositiveIntegerField('Количество подписчиков', null=True, blank=True)
-    subscriptions = models.PositiveIntegerField('Количество подписок', null=True, blank=True)
-    qty_posts = models.PositiveIntegerField('Количество постов', null=True, blank=True)
-    avg_amount_likes_on_all_posts = models.FloatField('Среднее количество лайков на всех постах', null=True, blank=True)
-    avg_amount_likes_on_last_20_posts = models.FloatField('Среднее количество лайков на последних 20 постах', null=True, blank=True)
+    stats = models.ForeignKey('PersonStats', on_delete=models.CASCADE)
 
     class MPTTMeta:
         order_insertion_by = ['full_name']
 
     def __str__(self):
         return self.full_name
+
+
+class PersonStats(models.Model):
+    qty_subscribers = models.PositiveIntegerField('Количество подписчиков', null=True, blank=True)
+    subscriptions = models.PositiveIntegerField('Количество подписок', null=True, blank=True)
+    qty_posts = models.PositiveIntegerField('Количество постов', null=True, blank=True)
+    avg_amount_likes_on_all_posts = models.FloatField('Среднее количество лайков на всех постах', null=True, blank=True)
+    avg_amount_likes_on_last_20_posts = models.FloatField('Среднее количество лайков на последних 20 постах', null=True,
+                                                          blank=True)
 
 
 class InstagramAccount(models.Model):
